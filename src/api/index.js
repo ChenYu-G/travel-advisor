83756e8d3fcc73b534e5d97a4f3e2557209518f8
ChenYu <cgeng.charlie@gmail.com>
2023-05-02 16:44:06 -0500

    update api_key

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,27 @@
-import { NextWeek } from "@material-ui/icons";
-import axios from "axios";
+import axios from 'axios'
 
 export const getPlacesData = async (type, sw, ne) => {
-  try {
-    const {
-      data: { data },
-    } = await axios.get(
-      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
-      {
-        params: {
-          bl_latitude: sw.lat,
-          tr_latitude: ne.lat,
-          bl_longitude: sw.lng,
-          tr_longitude: ne.lng,
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "57abde2903msh920e1e0d524fee3p11ac85jsnce6ca992b4be",
-          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
-        },
-      }
-    );
-    return data;
-  } catch (error) {
-    console.log(error);
-  }
-};
+	try {
+		const {
+			data: { data },
+		} = await axios.get(
+			`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+			{
+				params: {
+					bl_latitude: sw.lat,
+					tr_latitude: ne.lat,
+					bl_longitude: sw.lng,
+					tr_longitude: ne.lng,
+				},
+				headers: {
+					'X-RapidAPI-Key':
+						'817dc7c090msh6a8eb3409bb3fd2p17cfb1jsn9cec0fb71d89',
+					'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+				},
+			}
+		)
+		return data
+	} catch (error) {
+		console.log(error)
+	}
+}
